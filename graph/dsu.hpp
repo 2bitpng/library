3e8ac22f624027db352d6b6ac4c9ef52c572d30b
2bit <sasagawa@2bit.local>
2022-08-14 19:33:36 +0900

    make doc and dsu change to union by size

--- graph/dsu.hpp
+++ graph/dsu.hpp
@@ -3,40 +3,32 @@ using namespace std;
 using ll = long long;
 struct dsu{
   public:
-    dsu() : _n(0) {}
-    dsu (int n) :_n(n),_size(n,1),_par(n) {
-      for(int _i = 0; _i < _n; _i++){
-        _par[_i]=_i;
-      }
-    }
+    dsu() : _N(0) {}
+    dsu (int N) :_N(N),_data(N,-1) {}
 
     int root(int a){
-      while(_par[a]!=a){
-        a = _par[a] = _par[_par[a]];
-      }
-      return a;
+      if(_data[a]<0) return a;
+      return _data[a] = root(_data[a]);
     }
 
     bool same(int a,int b){
-      return root(a)==root(b);
+      return root(a) == root(b);
     }
 
     int size(int a){
-      return _size[root(a)];
+      return -_data[root(a)];
     }
 
     int merge(int a,int b){
       int x = root(a);
       int y = root(b);
-      if(x==y)return x;
-      if(_size[x]<_size[y])swap(x,y);
-      _size[x]+=_size[y];
-      _par[y]=x;
+      if(x == y)return x;
+      if(_data[x] > _data[y]) swap(x,y);
+      _data[x] += _data[y];
+      _data[y] = x;
       return x;
     }
-
   private:
-    int _n;  
-    vector<int> _size;
-    vector<int> _par;
+    int _N;  
+    vector<int> _data;
 };
